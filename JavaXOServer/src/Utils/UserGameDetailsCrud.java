5deca7e56286ba0180106201bea4b27fe5b63737
Ahmed gomaa <agomaa528.ag@gmail.com>
2021-12-30 11:36:45 +0200

    enhancement

--- JavaXOServer/src/Utils/UserGameDetailsCrud.java
+++ JavaXOServer/src/Utils/UserGameDetailsCrud.java
@@ -5,9 +5,7 @@
  */
 package Utils;
 
-import Entities.User;
 import Entities.UserGameDetails;
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.ByteArrayOutputStream;
 import java.io.DataInputStream;
@@ -15,8 +13,6 @@ import java.io.DataOutputStream;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import org.json.JSONException;
 import org.json.JSONObject;
 
@@ -26,13 +22,7 @@ import org.json.JSONObject;
  */
 public 
 class UserGameDetailsCrud implements ICrud<UserGameDetails>{
-DataOutputStream out;
-    DataInputStream in;
-
-    public UserGameDetailsCrud(DataInputStream in,DataOutputStream out) {
-        this.out = out;
-        this.in = in;
-    }
+    
     @Override
     public void add(UserGameDetails entity) throws JSONException, IOException{
         System.out.print(entity);
@@ -41,80 +31,83 @@ DataOutputStream out;
 
     @Override
     public void update(String id, Entities.UserGameDetails entity) throws JSONException, IOException {
-       
-            JSONObject jsonObject = new JSONObject(id);
-            String id2 = jsonObject.getString("id");
-            System.out.print("id: "+id2+"entity: "+entity);
-        
-            out.writeInt(1);
-        
+        JSONObject params = new JSONObject(id);
+
+        String id2 = params.getString("id");
+        System.out.print("id: "+id2+"entity: "+entity);
+
+        out.writeInt(1);
     }
 
     @Override
     public void delete(String id) throws JSONException, IOException{
-            JSONObject jsonObject = new JSONObject(id);
-            System.out.println(jsonObject.getString("id"));
-            out.writeInt(1);
-        
+        JSONObject params = new JSONObject(id);
+
+        System.out.println(params.getString("id"));
+
+        out.writeInt(1);
     }
 
     @Override
     public void get(String id) throws JSONException, IOException{
-         System.out.print("id: "+id);
-            JSONObject jsonObject = new JSONObject(id);
-            System.out.println(jsonObject.getString("id"));
-            
-            out.writeUTF(UserGameDetails.dumyObject().toJson());
-       
+        JSONObject params = new JSONObject(id);
+
+        System.out.print("id: "+id);
+        System.out.println(params.getString("id"));
+
+        out.writeUTF(UserGameDetails.dumyObject().toJson());
     }
 
     @Override
     public void getAll() throws JSONException, IOException{
-            final ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-            final ObjectMapper mapper = new ObjectMapper();
+        final ByteArrayOutputStream out2 = new ByteArrayOutputStream();
 
-            mapper.writeValue(out2,  new ArrayList(Arrays.asList(new UserGameDetails[]{UserGameDetails.dumyObject(),UserGameDetails.dumyObject()})));
 
-            final byte[] data = out2.toByteArray();
-            System.out.println(new String(data));
-    
-            out.writeUTF(new String(data));
-         
+        mapper.writeValue(out2,  new ArrayList(Arrays.asList(new UserGameDetails[]{UserGameDetails.dumyObject(),UserGameDetails.dumyObject()})));
+        final byte[] data = out2.toByteArray();
+        System.out.println(new String(data));
+        out.writeUTF(new String(data));
     }
     
     
     public void getAllWithId(String id) throws JSONException, IOException {
-         System.out.print("id: "+id);
-        JSONObject jsonObject = new JSONObject(id);
-        System.out.println(jsonObject.getString("id"));
         final ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-        final ObjectMapper mapper = new ObjectMapper();
+        JSONObject params = new JSONObject(id);
 
+        System.out.print("id: "+id);
+        System.out.println(params.getString("id"));
+
+         
+        
         mapper.writeValue(out2,  new ArrayList(Arrays.asList(new UserGameDetails[]{UserGameDetails.dumyObject(),UserGameDetails.dumyObject()})));
-
         final byte[] data = out2.toByteArray();
         System.out.println(new String(data));
-
         out.writeUTF(new String(data));
-      
     }
     
     
     public void getAllWithUserName(String userName) throws JSONException, IOException {
-         System.out.print("userName: "+userName);
-        JSONObject jsonObject = new JSONObject(userName);
-        System.out.println(jsonObject.getString("userName"));
         final ByteArrayOutputStream out2 = new ByteArrayOutputStream();
-        final ObjectMapper mapper = new ObjectMapper();
+        JSONObject jsonObject = new JSONObject(userName);
 
+        System.out.print("userName: "+userName);
+        System.out.println(jsonObject.getString("userName"));
+
+        
+        
         mapper.writeValue(out2,  new ArrayList(Arrays.asList(new UserGameDetails[]{UserGameDetails.dumyObject(),UserGameDetails.dumyObject()})));
-
         final byte[] data = out2.toByteArray();
         System.out.println(new String(data));
-
         out.writeUTF(new String(data));
 
     }
+    
+    public UserGameDetailsCrud(DataInputStream in,DataOutputStream out) {
+        this.out = out;
+        this.in = in;
+    }
    
-
+    private final ObjectMapper mapper = new ObjectMapper();
+    private DataOutputStream out;
+    private DataInputStream in;
 }
